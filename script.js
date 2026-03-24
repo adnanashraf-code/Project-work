50a5e336e97a75578c83dae0774c634996c10fe5
Adnan Ashraf <adnanashraf7205@gmail.com>
2026-03-25 03:11:38 +0530

    project work

---
+++ script.js
@@ -0,0 +1,15 @@
+let allelems = document.querySelectorAll(".elem");
+let fullelems = document.querySelectorAll(".fullelems");
+let btn = document.querySelectorAll(".fullelems .back");
+let main = document.querySelector("#main");
+allelems.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    fullelems[elem.id].style.display = "block";
+  });
+});
+
+btn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    fullelems[btn.id].style.display = "none";
+  });
+});
