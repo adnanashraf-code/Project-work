61ecb1e6fb894d7468a874d56612ffbd1f0daeb6
Adnan Ashraf <adnanashraf7205@gmail.com>
2026-03-29 23:17:44 +0530

    project work

--- script.js
+++ script.js
@@ -3,25 +3,55 @@ function openfeatures() {
   let fullelems = document.querySelectorAll(".fullelems");
   let btn = document.querySelectorAll(".fullelems .back");
   let main = document.querySelector("#main");
-  allelems.forEach(function (elem) {
+  allelems.forEach(function (elem, index) {
     elem.addEventListener("click", function () {
-      fullelems[elem.id].style.display = "block";
+      fullelems[index].style.display = "block";
     });
   });
 
-  btn.forEach(function (btn) {
+  btn.forEach(function (btn, index) {
     btn.addEventListener("click", function () {
-      fullelems[btn.id].style.display = "none";
+      fullelems[index].style.display = "none";
     });
   });
 }
 openfeatures();
 
+const allTasks = [
+  {
+    task: "Complete JavaScript revision",
+    details: "Revise closures, promises, async-await",
+    imp: true,
+  },
+  {
+    task: "Build To-Do App UI",
+    details: "Create responsive layout with HTML, CSS, JS",
+    imp: true,
+  },
+  {
+    task: "Practice DSA",
+    details: "Solve 5 problems on arrays and strings",
+    imp: false,
+  },
+  {
+    task: "Update LinkedIn",
+    details: "Post about 21-day frontend challenge",
+    imp: false,
+  },
+  {
+    task: "Learn API Integration",
+    details: "Fetch data using fetch() and display UI",
+    imp: true,
+  },
+];
+
 let form = document.querySelector(".addtask form");
-let Input = document.querySelector(".addtask form input");
+let Input = document.querySelector(".addtask form #task-input");
 let textarea = document.querySelector(".addtask form textarea");
+let checkbox = document.querySelector(".addtask form #check");
+let alltask = document.querySelector(".alltask");
+console.log(alltask);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
